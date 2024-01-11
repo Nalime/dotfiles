7f127ed77e41c170b045ca7cc19282a8102d8ceb
Nalime <tw.landy.lu@gmail.com>
2024-01-11 14:10:17 +0800

    Add a comma

--- nvim/.config/nvim/lua/nalime/plugins/init.lua
+++ nvim/.config/nvim/lua/nalime/plugins/init.lua
@@ -7,5 +7,5 @@ return {
 
     -- vim-bbye: Better buffer closing
     -- Main use is to prevent also closing the displaying window
-    { "moll/vim-bbye", event = "VeryLazy" }
+    { "moll/vim-bbye", event = "VeryLazy" },
 }
