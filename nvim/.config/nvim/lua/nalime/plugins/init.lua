a2d0449cfbc81a09217638afab9a29cc7964d8dc
Nalime <tw.landy.lu@gmail.com>
2024-01-11 17:47:01 +0800

    Add disabled treesitter modules

--- nvim/.config/nvim/lua/nalime/plugins/init.lua
+++ nvim/.config/nvim/lua/nalime/plugins/init.lua
@@ -1,10 +1,4 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter-context",
-        dependencies = "nvim-treesitter/nvim-treesitter",
-        event = "VeryLazy",
-    },
-
     -- vim-bbye: Better buffer closing
     -- Main use is to prevent also closing the displaying window
     { "moll/vim-bbye", event = "VeryLazy" },
---
+++ nvim/.config/nvim/lua/nalime/plugins/treesitter-textobjects.lua
@@ -0,0 +1,54 @@
+return {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    event = "VeryLazy",
+    enabled = false,
+
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            textobjects = {
+                select = {
+                    enable = true,
+
+                    -- Automatically jump forward to textobj, similar to targets.vim
+                    lookahead = true,
+
+                    keymaps = {
+                        -- You can use the capture groups defined in textobjects.scm
+
+                        ["am"] = { query = "@function.outer", desc = "Select outer part of a method/function definition" },
+                        ["im"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
+
+                        ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
+                        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
+                    },
+                },
+                move = {
+                    enable = true,
+                    set_jumps = true, -- whether to set jumps in the jumplist
+                    goto_next_start = {
+                        ["]m"] = { query = "@function.outer", desc = "Next method/function def start" },
+                        ["]c"] = { query = "@class.outer", desc = "Next class start" },
+
+                        -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+                        -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+                        -- ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+                        -- ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+                    },
+                    goto_next_end = {
+                        ["]M"] = { query = "@function.outer", desc = "Next method/function def end" },
+                        ["]C"] = { query = "@class.outer", desc = "Next class end" },
+                    },
+                    goto_previous_start = {
+                        ["[m"] = { query = "@function.outer", desc = "Prev method/function def start" },
+                        ["[c"] = { query = "@class.outer", desc = "Prev class start" },
+                    },
+                    goto_previous_end = {
+                        ["[M"] = { query = "@function.outer", desc = "Prev method/function def end" },
+                        ["[C"] = { query = "@class.outer", desc = "Prev class end" },
+                    },
+                },
+            },
+        })
+    end,
+}
