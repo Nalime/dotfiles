10511ddbf11464c70309d3f7e54a7836cef24e87
Landy Lu <tw.landy.lu@gmail.com>
2024-01-16 20:58:28 +0800

    Add Neovim machine-local configs

--- nvim/.config/nvim/lua/nalime/init.lua
+++ nvim/.config/nvim/lua/nalime/init.lua
@@ -8,7 +8,12 @@ vim.loader.enable()
 -- ./general/
 require("nalime.general.mappings")
 require("nalime.general.options")
-require("nalime.general.lazy") -- ordering
+require("nalime.general.lazy")
+
+print(vim.fn.stdpath("config"))
+if vim.fn.filereadable(vim.fn.stdpath("config") .. "/lua/nalime/local.lua") then
+    require("nalime.general.local")
+end
 
 -- Plugins
 -- ./plugins/
