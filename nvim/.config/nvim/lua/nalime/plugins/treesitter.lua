8119a4d99a40006b2465369eab29931b5227823c
Landy Lu <tw.landy.lu@gmail.com>
2024-01-16 21:08:33 +0800

    Change nvim-treesitter to install everything

--- nvim/.config/nvim/lua/nalime/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/nalime/plugins/treesitter.lua
@@ -2,17 +2,19 @@ return {
     -- nvim-treesitter: Online parser in Neovim
     -- Syntax highlighting and more
     "nvim-treesitter/nvim-treesitter",
+    main = "nvim-treesitter.configs",
     version = "*",
     event = "VeryLazy",
 
     opts = {
-        ensure_installed = {
-            "c",
-            "cpp",
-            "lua",
-            "python",
-        },
-        auto_install = true,
+        ensure_installed = "all",
+        -- ensure_installed = {
+        --     "c",
+        --     "cpp",
+        --     "lua",
+        --     "python",
+        -- },
+        auto_install = false,
         highlight = {
             enable = true,
             additional_vim_regex_highlighting = { "markdown" },
