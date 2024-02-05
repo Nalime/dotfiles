6d1225fca2810337dbab788f393fdae7f89f4511
Nalime <tw.landy.lu@gmail.com>
2024-02-05 13:11:55 +0800

    Change lualine to show relative paths

--- nvim/.config/nvim/lua/nalime/plugins/lualine.lua
+++ nvim/.config/nvim/lua/nalime/plugins/lualine.lua
@@ -26,7 +26,9 @@ return {
         sections = {
             lualine_a = { "mode" },
             lualine_b = { "branch", "diff", "diagnostics" },
-            lualine_c = { "filename" },
+            lualine_c = {
+                { "filename", path = 1 },
+            },
             lualine_x = { spaces, "encoding", "fileformat", "filetype" },
             lualine_y = { "progress" },
             lualine_z = { "location" },
