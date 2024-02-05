28e2f59a2357837d113d86e8e53a227b2910b25f
Nalime <tw.landy.lu@gmail.com>
2024-02-05 20:17:36 +0800

    Tweak hardtime configs

--- nvim/.config/nvim/lua/nalime/plugins/hardtime.lua
+++ nvim/.config/nvim/lua/nalime/plugins/hardtime.lua
@@ -4,12 +4,23 @@ return {
     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
 
     opts = {
-        max_time = 500,
-        max_count = 3,
+        max_time = 1000,
+        max_count = 2,
         disable_mouse = false,
         allow_different_key = true,
 
-        disabled_keys = {},
+        restricted_keys = {
+            ["<Up>"] = { "n", "x" },
+            ["<Down>"] = { "n", "x" },
+            ["<Left>"] = { "n", "x" },
+            ["<Right>"] = { "n", "x" },
+        },
+        disabled_keys = {
+            ["<Up>"] = {},
+            ["<Down>"] = {},
+            ["<Left>"] = {},
+            ["<Right>"] = {},
+        },
 
         disabled_filetypes = {
             "alpha",
