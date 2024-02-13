348968babcb6d02b9f2dc982ada66382771ae92b
Nalime <tw.landy.lu@gmail.com>
2024-02-14 00:52:09 +0800

    Change nvim-colorizer fork

--- nvim/.config/nvim/lua/nalime/plugins/colorizer.lua
+++ nvim/.config/nvim/lua/nalime/plugins/colorizer.lua
@@ -1,6 +1,12 @@
 return {
-    "norcalli/nvim-colorizer.lua",
+    "NvChad/nvim-colorizer.lua",
     main = "colorizer",
 
-    opts = { "*" },
+    opts = {
+        user_default_options = {
+            RRGGBBAA = true, -- #RRGGBBAA hex codes
+            AARRGGBB = true, -- 0xAARRGGBB hex codes
+            css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        },
+    },
 }
