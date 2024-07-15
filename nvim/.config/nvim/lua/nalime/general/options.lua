e2c55fc3f38a025fb590e29b958968f0fa7b57ed
Nalime <tw.landy.lu@gmail.com>
2024-07-15 17:53:36 +0800

    Disable Neovim providers

--- nvim/.config/nvim/lua/nalime/general/options.lua
+++ nvim/.config/nvim/lua/nalime/general/options.lua
@@ -44,3 +44,9 @@ vim.opt.showmode = false
 
 -- Cursorline (for plugins)
 vim.opt.cursorline = true
+
+-- Disable providers
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
