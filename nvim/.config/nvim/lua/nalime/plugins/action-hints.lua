99239a2bb8198fc9f7ee81c0b28ee665caae674e
Nalime <tw.landy.lu@gmail.com>
2026-07-20 01:35:03 +0800

    Add action-hints Neovim plugin

---
+++ nvim/.config/nvim/lua/nalime/plugins/action-hints.lua
@@ -0,0 +1,9 @@
+return {
+    -- action-hints: Indicate availability of definition and references
+    "roobert/action-hints.nvim",
+    event = "VeryLazy",
+
+    opts = {
+        use_virtual_text = true,
+    },
+}
