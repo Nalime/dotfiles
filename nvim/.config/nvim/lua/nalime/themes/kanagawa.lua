61a21221f0b5a2a0e6e57cc0628c5e1765175e99
Nalime <tw.landy.lu@gmail.com>
2026-07-20 00:27:36 +0800

    Configure kanagawa

--- nvim/.config/nvim/lua/nalime/themes/kanagawa.lua
+++ nvim/.config/nvim/lua/nalime/themes/kanagawa.lua
@@ -1,29 +1,43 @@
 return {
     -- kanagawa colorscheme
     "rebelot/kanagawa.nvim",
-    -- name = "catppuccin",
     priority = 1000,
 
     config = function()
         local kanagawa = require('kanagawa')
 
-        require('kanagawa').setup({
-            compile = false,             -- enable compiling the colorscheme
-            undercurl = true,            -- enable undercurls
+        require("kanagawa").setup({
+            compile = false,
+            undercurl = true,
             commentStyle = { italic = true },
             functionStyle = {},
-            keywordStyle = { italic = true},
+            keywordStyle = { italic = true },
             statementStyle = { bold = true },
             typeStyle = {},
-            transparent = false,         -- do not set background color
-            dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
-            terminalColors = true,       -- define vim.g.terminal_color_{0,17}
-            colors = {                   -- add/modify theme and palette colors
+            transparent = true,
+            dimInactive = false,
+            terminalColors = true,
+            colors = {
                 palette = {},
-                theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+                theme = {
+                    all = {
+                        ui = {
+                            bg_gutter = "none"
+                        }
+                    }
+                },
             },
-            overrides = function(colors) -- add/modify highlights
-                return {}
+            overrides = function(colors)
+                local theme = colors.theme
+                return {
+                    TelescopeTitle = { fg = theme.ui.special, bold = true },
+                    TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+                    TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+                    TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+                    TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+                    TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+                    TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+                }
             end,
             theme = "wave",              -- Load "wave" theme
             background = {               -- map the value of 'background' option to a theme
